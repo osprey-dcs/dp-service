72e2cc13a67c7aa69fbae4bf198bf77eda9bfbfe
Craig McChesney <auto@craigmcc.com>
2024-01-15 14:01:10 -0700

    dp-service #19: mechanism for verification of ingestion database artifacts in integration test

--- src/test/java/com/ospreydcs/dp/service/integration/IntegrationGrpcTest.java
+++ src/test/java/com/ospreydcs/dp/service/integration/IntegrationGrpcTest.java
@@ -3,6 +3,9 @@ package com.ospreydcs.dp.service.integration;
 import com.ospreydcs.dp.grpc.v1.common.DataTable;
 import com.ospreydcs.dp.grpc.v1.ingestion.IngestionRequest;
 import com.ospreydcs.dp.grpc.v1.ingestion.IngestionResponse;
+import com.ospreydcs.dp.service.common.bson.BucketDocument;
+import com.ospreydcs.dp.service.common.bson.RequestStatusDocument;
+import com.ospreydcs.dp.service.common.mongo.MongoSyncClient;
 import com.ospreydcs.dp.service.ingest.IngestionTestBase;
 import com.ospreydcs.dp.service.ingest.benchmark.BenchmarkStreamingIngestion;
 import com.ospreydcs.dp.service.ingest.benchmark.IngestionBenchmarkBase;
@@ -16,6 +19,7 @@ import io.grpc.inprocess.InProcessServerBuilder;
 import io.grpc.testing.GrpcCleanupRule;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.bson.conversions.Bson;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
@@ -23,11 +27,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.time.Instant;
+import java.util.*;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.eq;
+import static java.lang.Thread.sleep;
 import static org.junit.Assert.*;
 import static org.mockito.AdditionalAnswers.delegatesTo;
 import static org.mockito.Mockito.mock;
@@ -35,15 +42,65 @@ import static org.mockito.Mockito.mock;
 @RunWith(JUnit4.class)
 public class IntegrationGrpcTest extends IngestionTestBase {
 
+    // static variables
     private static final Logger LOGGER = LogManager.getLogger();
+    private static final IntegrationTestMongoClient MONGO_CLIENT = new IntegrationTestMongoClient();
+
+    private static class IntegrationTestMongoClient extends MongoSyncClient {
+
+        private static final int MONGO_FIND_RETRY_COUNT = 300;
+        private static final int MONGO_FIND_RETRY_INTERVAL_MILLIS = 100;
+
+        public BucketDocument findBucket(String id) {
+            for (int retryCount = 0 ; retryCount < MONGO_FIND_RETRY_COUNT ; ++retryCount){
+                List<BucketDocument> matchingBuckets = new ArrayList<>();
+                mongoCollectionBuckets.find(eq("_id", id)).into(matchingBuckets);
+                if (matchingBuckets.size() > 0) {
+                    return matchingBuckets.get(0);
+                } else {
+                    try {
+                        LOGGER.info("findBucket id: " + id + " retrying");
+                        Thread.sleep(MONGO_FIND_RETRY_INTERVAL_MILLIS);
+                    } catch (InterruptedException ex) {
+                        // ignore and just retry
+                    }
+                }
+            }
+            return null;
+        }
+
+        public RequestStatusDocument findRequestStatus(Integer providerId, String requestId) {
+            for (int retryCount = 0 ; retryCount < MONGO_FIND_RETRY_COUNT ; ++retryCount) {
+                List<RequestStatusDocument> matchingDocuments = new ArrayList<>();
+                Bson filter = and(eq("providerId", providerId), eq("requestId", requestId));
+                mongoCollectionRequestStatus.find(filter).into(matchingDocuments);
+                if (matchingDocuments.size() > 0) {
+                    return matchingDocuments.get(0);
+                } else {
+                    try {
+                        LOGGER.info("findRequestStatus providerId: " + providerId
+                                + " requestId: " + requestId
+                                + " retrying");
+                        Thread.sleep(MONGO_FIND_RETRY_INTERVAL_MILLIS);
+                    } catch (InterruptedException ex) {
+                        // ignore and just retry
+                    }
+                }
+            }
+            return null;
+        }
+
+    }
 
     private static class IntegrationTestStreamingIngestionApp extends BenchmarkStreamingIngestion {
 
         private static class IntegrationTestIngestionRequestInfo {
             public final int providerId;
+            public final long startSeconds;
             public boolean responseReceived = false;
-            public IntegrationTestIngestionRequestInfo(int providerId) {
+            public IntegrationTestIngestionRequestInfo(int providerId, long startSeconds) {
                 this.providerId = providerId;
+                this.startSeconds = startSeconds;
             }
         }
 
@@ -51,7 +108,7 @@ public class IntegrationGrpcTest extends IngestionTestBase {
                 extends BenchmarkStreamingIngestion.StreamingIngestionTask
         {
             // instance variables
-            private Map<String, IntegrationTestIngestionRequestInfo> requestValidationMap = new HashMap<>();
+            private Map<String, IntegrationTestIngestionRequestInfo> requestValidationMap = new TreeMap<>();
             private final ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock();
             private final Lock readLock = rwLock.readLock();
             private final Lock writeLock = rwLock.writeLock();
@@ -75,7 +132,14 @@ public class IntegrationGrpcTest extends IngestionTestBase {
                     // add an entry for the request to the validation map
                     requestValidationMap.put(
                             request.getClientRequestId(),
-                            new IntegrationTestIngestionRequestInfo(request.getProviderId()));
+                            new IntegrationTestIngestionRequestInfo(
+                                    request.getProviderId(),
+                                    request
+                                            .getDataTable()
+                                            .getDataTimeSpec()
+                                            .getFixedIntervalTimestampSpec()
+                                            .getStartTime()
+                                            .getEpochSeconds()));
 
                 } finally {
                     // using try...finally to make sure we unlock!
@@ -123,6 +187,55 @@ public class IntegrationGrpcTest extends IngestionTestBase {
 
             }
 
+            private void verifyRequestDbArtifacts(
+                    String requestId, IntegrationTestIngestionRequestInfo requestInfo
+            ) {
+                // verify request status
+                RequestStatusDocument statusDocument =
+                        MONGO_CLIENT.findRequestStatus(requestInfo.providerId, requestId);
+                assertEquals(params.numColumns, statusDocument.getIdsCreated().size());
+
+                // verify buckets
+                for (int colIndex = params.firstColumnIndex; colIndex <= params.lastColumnIndex; colIndex++) {
+                    final String columnName = NAME_COLUMN_BASE + colIndex;
+                    final String bucketId = columnName + "-" + requestInfo.startSeconds + "-0";
+                    assertTrue(
+                            "providerId: " + requestInfo.providerId
+                                    + " requestId: " + requestId
+                                    + " bucketId: " + bucketId,
+                            statusDocument.getIdsCreated().contains(bucketId));
+                    BucketDocument bucketDocument = MONGO_CLIENT.findBucket(bucketId);
+                    assertNotNull("bucketId: " + bucketId, bucketDocument);
+                    assertEquals(columnName, bucketDocument.getColumnName());
+                    assertEquals(bucketId, bucketDocument.getId());
+                    assertEquals(params.numRows, bucketDocument.getNumSamples());
+                    assertEquals(1000000, bucketDocument.getSampleFrequency());
+                    assertEquals(requestInfo.startSeconds, bucketDocument.getFirstSeconds());
+                    assertEquals(0, bucketDocument.getFirstNanos());
+                    assertEquals(
+                            Date.from(Instant.ofEpochSecond(requestInfo.startSeconds, 0L)),
+                            bucketDocument.getFirstTime());
+                    assertEquals(requestInfo.startSeconds, bucketDocument.getLastSeconds());
+                    assertEquals(999000000L, bucketDocument.getLastNanos());
+                    assertEquals(
+                            Date.from(Instant.ofEpochSecond(requestInfo.startSeconds, 999000000L)),
+                            bucketDocument.getLastTime());
+                    // TODO: why does getDataType() return null? can't find any details about it
+                    //  assertEquals("DOUBLE", bucketDocument.getDataType());
+                    assertEquals("calibration test", bucketDocument.getEventDescription());
+                    assertEquals(params.startSeconds, bucketDocument.getEventSeconds());
+                    assertEquals(0, bucketDocument.getEventNanos());
+                    assertTrue(bucketDocument.getAttributeMap().get("sector").equals("07"));
+                    assertTrue(bucketDocument.getAttributeMap().get("subsystem").equals("vacuum"));
+                    assertEquals(params.numRows, bucketDocument.getColumnDataList().size());
+                    // TODO: verify each value
+                    for (int valIndex = 0 ; valIndex < bucketDocument.getNumSamples() ; ++valIndex) {
+                        final double doubleValue = valIndex + (double) valIndex / bucketDocument.getNumSamples();
+                        assertEquals(doubleValue, bucketDocument.getColumnDataList().get(valIndex));
+                    }
+                }
+            }
+
             @Override
             protected void onCompleted() {
 
@@ -132,10 +245,12 @@ public class IntegrationGrpcTest extends IngestionTestBase {
                 try {
                     // iterate through requestMap and make sure all requests were acked/verified
                     for (var entry : requestValidationMap.entrySet()) {
+                        String requestId = entry.getKey();
                         IntegrationTestIngestionRequestInfo requestInfo = entry.getValue();
                         if (!requestInfo.responseReceived) {
                             fail("did not receive ack for request: " + entry.getKey());
                         }
+                        verifyRequestDbArtifacts(requestId, requestInfo);
                     }
 
                 } finally {
@@ -207,6 +322,9 @@ public class IntegrationGrpcTest extends IngestionTestBase {
     @BeforeClass
     public static void setUp() throws Exception {
 
+        // init the mongo client interface for db verification
+        MONGO_CLIENT.init();
+
         // uncomment line below to use a real handler, and write to database
 //        IngestionHandlerInterface handler = new MongoSyncHandler();
         IngestionHandlerInterface handler = MongoIngestionHandler.newMongoSyncIngestionHandler();
@@ -235,6 +353,7 @@ public class IntegrationGrpcTest extends IngestionTestBase {
     public static void tearDown() {
         serviceImpl = null;
         client = null;
+        MONGO_CLIENT.fini();
     }
 
     /**
