7638e2b911f9ccbc983612d9c0285917a6efc907
allenck <allenck@192.168.1.24>
2024-03-05 10:00:22 -0500

    Removed unused import - javax.xml.cryto.Data - showed compile error?

--- src/main/java/com/ospreydcs/dp/service/query/service/QueryServiceImpl.java
+++ src/main/java/com/ospreydcs/dp/service/query/service/QueryServiceImpl.java
@@ -13,8 +13,6 @@ import io.grpc.stub.StreamObserver;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import javax.xml.crypto.Data;
-
 public class QueryServiceImpl extends DpQueryServiceGrpc.DpQueryServiceImplBase {
 
     // static variables
