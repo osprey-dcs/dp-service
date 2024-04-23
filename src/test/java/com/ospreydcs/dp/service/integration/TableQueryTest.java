8cba1fb402055dd1f9db81dcf2a3085ef9f92400
Craig McChesney <auto@craigmcc.com>
2024-04-23 11:54:06 -0600

    dp-service #62: additional queryTable test coverage

--- src/test/java/com/ospreydcs/dp/service/integration/TableQueryTest.java
+++ src/test/java/com/ospreydcs/dp/service/integration/TableQueryTest.java
@@ -93,6 +93,31 @@ public class TableQueryTest extends GrpcIntegrationTestBase {
         }
 
         {
+            // test table query API with query time range offset from bucket begin/end times, column-oriented result
+
+            // send table query for 5-second subset of ingested data,
+            // starting one second offset from start of ingestion data
+            final int queryNumSeconds = 5;
+            final long queryStartSeconds = startSeconds + 1;
+            final long queryStartNanos = 499_000_000L;  // start end middle of bucket
+            final long queryEndSeconds = queryStartSeconds + queryNumSeconds;
+            final long queryEndNanos = 499_000_000L;  // end in middle of bucket
+            final List<String> queryColumnNames = List.of("S01-GCC01", "S02-BPM02");
+
+            // still expect table with 50 rows, one for every tenth of a second, but offset from bucket times
+            final int numRowsExpected = 10 * 5;
+
+            sendAndVerifyQueryTablePvNameListColumnResult(
+                    numRowsExpected,
+                    queryColumnNames,
+                    queryStartSeconds,
+                    queryStartNanos,
+                    queryEndSeconds,
+                    queryEndNanos,
+                    validationMap);
+        }
+
+        {
             // test table query API with pvNameList and row-oriented result
 
             // send table query for 5-second subset of ingested data,
@@ -117,5 +142,58 @@ public class TableQueryTest extends GrpcIntegrationTestBase {
                     validationMap);
         }
 
+        {
+            // test table query API with pvNamePattern and row-oriented result
+
+            // send table query for 5-second subset of ingested data,
+            // starting one second offset from start of ingestion data
+            final int queryNumSeconds = 5;
+            final long queryStartSeconds = startSeconds + 1;
+            final long queryStartNanos = 0;
+            final long queryEndSeconds = queryStartSeconds + queryNumSeconds;
+            final long queryEndNanos = 0;
+            final String pvNamePattern = "S01";
+            final List<String> expectedPvNameMatches =
+                    List.of("S01-GCC01", "S01-GCC02", "S01-GCC03", "S01-BPM01", "S01-BPM02", "S01-BPM03");
+
+            // 5 buckets each with 10 rows for each PV, timestamps are aligned
+            final int numRowsExpected = 10 * 5;
+
+            sendAndVerifyQueryTablePvNamePatternRowResult(
+                    numRowsExpected,
+                    pvNamePattern,
+                    expectedPvNameMatches,
+                    queryStartSeconds,
+                    queryStartNanos,
+                    queryEndSeconds,
+                    queryEndNanos,
+                    validationMap);
+        }
+
+        {
+            // test table query API with query time range offset from bucket begin/end times, row-oriented result
+
+            // send table query for 5-second subset of ingested data,
+            // starting one second offset from start of ingestion data
+            final int queryNumSeconds = 5;
+            final long queryStartSeconds = startSeconds + 1;
+            final long queryStartNanos = 499_000_000L;  // start end middle of bucket
+            final long queryEndSeconds = queryStartSeconds + queryNumSeconds;
+            final long queryEndNanos = 499_000_000L;  // end in middle of bucket
+            final List<String> queryColumnNames = List.of("S01-GCC01", "S02-BPM02");
+
+            // still expect table with 50 rows, one for every tenth of a second, but offset from bucket times
+            final int numRowsExpected = 10 * 5;
+
+            sendAndVerifyQueryTablePvNameListRowResult(
+                    numRowsExpected,
+                    queryColumnNames,
+                    queryStartSeconds,
+                    queryStartNanos,
+                    queryEndSeconds,
+                    queryEndNanos,
+                    validationMap);
+        }
     }
+
 }
