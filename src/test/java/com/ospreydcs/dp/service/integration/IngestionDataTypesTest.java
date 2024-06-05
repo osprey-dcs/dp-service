68b912ebbd8c5ab9894c34f11575d06faeee4d24
Craig McChesney <auto@craigmcc.com>
2024-06-05 13:47:05 -0600

    dp-service #65: ingestion and query test coverage for image data type

--- src/test/java/com/ospreydcs/dp/service/integration/IngestionDataTypesTest.java
+++ src/test/java/com/ospreydcs/dp/service/integration/IngestionDataTypesTest.java
@@ -1,5 +1,6 @@
 package com.ospreydcs.dp.service.integration;
 
+import com.google.protobuf.ByteString;
 import com.ospreydcs.dp.grpc.v1.common.*;
 import com.ospreydcs.dp.grpc.v1.ingestion.IngestDataRequest;
 import com.ospreydcs.dp.grpc.v1.query.QueryDataResponse;
@@ -11,6 +12,9 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.util.*;
 
 import static org.junit.Assert.*;
@@ -75,6 +79,7 @@ public class IngestionDataTypesTest extends GrpcIntegrationTestBase {
         final long startSeconds = configMgr().getConfigLong(CFG_KEY_START_SECONDS, DEFAULT_START_SECONDS);
         final long startNanos = 0L;
         final int providerId = INGESTION_PROVIDER_ID;
+
 
         List<DataColumn> arrayDataColumnList = null;
         Map<DataColumn, List<ArrayDataValueModel>> arrayValidationMap = null;
@@ -201,6 +206,114 @@ public class IngestionDataTypesTest extends GrpcIntegrationTestBase {
             verifyDataBucket(responseBucket, requestColumn, startSeconds, startNanos, samplePeriod, numSamples);
         }
 
+
+
+        ByteString imageByteString = null;
+        {
+            // ingest Image data type
+            try {
+                InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("test-image.bmp");
+                imageByteString = ByteString.readFrom(inputStream);
+            } catch (IOException ex) {
+                fail("error loading test-image.bmp: " + ex.getMessage());
+            }
+
+            // create IngestionRequestParams
+            final String requestId = "request-3";
+            final List<String> pvNames = Arrays.asList(
+                    "image_pv_01", "image_pv_02", "image_pv_03", "image_pv_04", "image_pv_05");
+            final int numPvs = pvNames.size();
+            final int numSamples = 5;
+            final long samplePeriod = 1_000_000_000L / numSamples;
+            final long endSeconds = startSeconds;
+            final long endNanos = samplePeriod * (numSamples - 1);
+            final IngestionTestBase.IngestionRequestParams params =
+                    new IngestionTestBase.IngestionRequestParams(
+                            providerId,
+                            requestId,
+                            null,
+                            null,
+                            null,
+                            null,
+                            startSeconds,
+                            startNanos,
+                            samplePeriod, // 5 values per second
+                            numSamples, // each DataColumn must contain 5 DataValues
+                            pvNames,
+                            null,
+                            null);
+
+            // build list of DataColumns
+            final List<DataColumn> dataColumnList = new ArrayList<>();
+            for(int colIndex = 0 ; colIndex < numPvs ; ++colIndex) {
+
+                final DataColumn.Builder dataColumnBuilder = DataColumn.newBuilder();
+                final String pvName = pvNames.get(colIndex);
+                dataColumnBuilder.setName(pvName);
+
+                for (int rowIndex = 0 ; rowIndex < numSamples ; ++rowIndex) {
+                    final DataValue.Builder imageDataValueBuilder = DataValue.newBuilder();
+                    final Image image =
+                            Image.newBuilder().setFileType(Image.FileType.BMP).setImage(imageByteString).build();
+                    imageDataValueBuilder.setImageValue(image);
+                    imageDataValueBuilder.build();
+                    dataColumnBuilder.addDataValues(imageDataValueBuilder);
+                }
+
+                DataColumn dataColumn = dataColumnBuilder.build();
+                dataColumnList.add(dataColumn);
+            }
+
+            // build request
+            final IngestDataRequest ingestionRequest =
+                    IngestionTestBase.buildIngestionRequest(params, dataColumnList);
+
+            // send request
+            final List<BucketDocument> bucketDocumentList=
+                    sendAndVerifyIngestDataStream(params, ingestionRequest, dataColumnList);
+
+            // verify contents of mongo bucket documents - the comparison of DataColumns in request to those in
+            // bucket document by sendAndVerifyIngestDataStream() is probably sufficient, but checking that the
+            // data values are as expected
+            int columnIndex = 0;
+            for (BucketDocument bucketDocument : bucketDocumentList) {
+
+                final DataColumn requestColumn = dataColumnList.get(columnIndex);
+                final DataColumn bucketColumn = bucketDocument.readDataColumnContent();
+
+                int rowIndex = 0;
+                for (DataValue bucketDataValue : bucketColumn.getDataValuesList()) {
+                    assertTrue(bucketDataValue.hasImageValue());
+                    final Image bucketImage = bucketDataValue.getImageValue();
+                    assertEquals(imageByteString, bucketImage.getImage());
+
+                    rowIndex = rowIndex + 1;
+                }
+
+                columnIndex = columnIndex + 1;
+            }
+
+            // perform query for single pv and verify results
+            final List<String> queryPvNames = Arrays.asList("image_pv_01");
+            final DataColumn requestColumn = dataColumnList.get(0);
+            final List<QueryDataResponse.QueryData.DataBucket> queryBuckets = queryDataStream(
+                    queryPvNames, startSeconds, startNanos, endSeconds, endNanos);
+            assertEquals(queryPvNames.size(), queryBuckets.size());
+            final QueryDataResponse.QueryData.DataBucket responseBucket = queryBuckets.get(0);
+            verifyDataBucket(responseBucket, requestColumn, startSeconds, startNanos, samplePeriod, numSamples);
+
+            // write image content from query to file
+            File outputFile = new File("src/test/resources/test-image-output.bmp");
+            try {
+                FileOutputStream outputStream = new FileOutputStream(outputFile);
+                outputStream.write(
+                        responseBucket.getDataColumn().getDataValues(0).getImageValue().getImage().toByteArray());
+            } catch (IOException ex) {
+                fail("error writing test-image-output.bmp");
+            }
+        }
+
+
         {
             // ingest structure data
 
@@ -211,6 +324,7 @@ public class IngestionDataTypesTest extends GrpcIntegrationTestBase {
             final boolean BOOLEAN_VALUE = false;
             final String DOUBLE_MEMBER = "double";
             final double DOUBLE_VALUE = 3.14;
+            final String IMAGE_MEMBER = "image";
             final String INTEGER_MEMBER = "integer";
             final int INTEGER_VALUE = 42;
             final String TIMESTAMP_MEMBER = "timestamp";
@@ -275,6 +389,14 @@ public class IngestionDataTypesTest extends GrpcIntegrationTestBase {
                             Structure.Field.newBuilder().setName(DOUBLE_MEMBER).setValue(doubleDataValue).build();
                     structureBuilder.addFields(doubleField);
 
+                    // add image
+                    final Image image =
+                            Image.newBuilder().setFileType(Image.FileType.BMP).setImage(imageByteString).build();
+                    final DataValue imageDataValue = DataValue.newBuilder().setImageValue(image).build();
+                    final Structure.Field imageField =
+                            Structure.Field.newBuilder().setName(IMAGE_MEMBER).setValue(imageDataValue).build();
+                    structureBuilder.addFields(imageField);
+
                     // add nested structure with integer and timestamp
                     
                     final DataValue.Builder nestedStructureValueBuilder = DataValue.newBuilder();
@@ -303,8 +425,6 @@ public class IngestionDataTypesTest extends GrpcIntegrationTestBase {
                             .setValue(nestedStructureValueBuilder)
                             .build();
                     structureBuilder.addFields(nestedStructureField);
-
-                    // add image
 
                     // get array from array ingestion DataColumn list and add to structure
                     final DataColumn arrayDataColumn = arrayDataColumnList.get(colIndex);
@@ -393,6 +513,13 @@ public class IngestionDataTypesTest extends GrpcIntegrationTestBase {
                                         0.0);
                                 break;
 
+                            case IMAGE_MEMBER:
+                                assertTrue(bucketStructureFieldDataValue.hasImageValue());
+                                final Image bucketImage = bucketStructureFieldDataValue.getImageValue();
+                                assertEquals(Image.FileType.BMP, bucketImage.getFileType());
+                                assertEquals(imageByteString, bucketImage.getImage());
+                                break;
+
                             case STRUCTURE_MEMBER:
                                 assertTrue(bucketStructureFieldDataValue.hasStructureValue());
                                 final Structure nestedBucketStructure =
@@ -451,6 +578,7 @@ public class IngestionDataTypesTest extends GrpcIntegrationTestBase {
             final QueryDataResponse.QueryData.DataBucket responseBucket = queryBuckets.get(0);
             verifyDataBucket(responseBucket, requestColumn, startSeconds, startNanos, samplePeriod, numSamples);
         }
+
     }
 
     private void verify2DArray(List<List<String>> array2D, Array bucketArray) {
