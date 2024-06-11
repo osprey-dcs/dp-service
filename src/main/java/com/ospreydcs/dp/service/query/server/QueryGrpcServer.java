e475d3f892ad8277029d2f4965f2da0f27000c06
Craig McChesney <auto@craigmcc.com>
2024-06-11 14:59:01 -0600

    dp-service #72: refactor query server to extend GrpcServerBase framework

--- src/main/java/com/ospreydcs/dp/service/query/server/QueryGrpcServer.java
+++ src/main/java/com/ospreydcs/dp/service/query/server/QueryGrpcServer.java
@@ -1,107 +1,64 @@
 package com.ospreydcs.dp.service.query.server;
 
-import com.ospreydcs.dp.service.common.config.ConfigurationManager;
+import com.ospreydcs.dp.service.common.server.GrpcServerBase;
 import com.ospreydcs.dp.service.query.handler.interfaces.QueryHandlerInterface;
 import com.ospreydcs.dp.service.query.handler.mongo.MongoQueryHandler;
 import com.ospreydcs.dp.service.query.service.QueryServiceImpl;
-import io.grpc.Grpc;
-import io.grpc.InsecureServerCredentials;
-import io.grpc.Server;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
-public class QueryGrpcServer {
+public class QueryGrpcServer extends GrpcServerBase {
 
     private static final Logger LOGGER = LogManager.getLogger();
-
-    // constants
-    private static final int TIMEOUT_TERMINATION_SECS = 30;
 
     // configuration
     public static final String CFG_KEY_PORT = "QueryServer.port";
     public static final int DEFAULT_PORT = 50052;
 
-    private Server server;
-    private QueryServiceImpl serviceImpl;
+    // instance variables
+    private final QueryServiceImpl serviceImpl;
 
-    private static ConfigurationManager configMgr() {
-        return ConfigurationManager.getInstance();
+    public QueryGrpcServer(QueryServiceImpl serviceImpl) {
+        super(serviceImpl);
+        this.serviceImpl = serviceImpl;
     }
 
-    protected void start() throws IOException {
-
-        initService();
-
-        int port = configMgr().getConfigInteger(CFG_KEY_PORT, DEFAULT_PORT);
-        server = Grpc.newServerBuilderForPort(port, InsecureServerCredentials.create())
-                .addService(serviceImpl)
-                .build()
-                .start();
-        LOGGER.info("Server started, listening on " + port);
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            @Override
-            public void run() {
-                // Use stderr here since the logger may have been reset by its JVM shutdown hook.
-                System.err.println("*** shutting down gRPC server since JVM is shutting down");
-                try {
-                    QueryGrpcServer.this.stop();
-                } catch (InterruptedException e) {
-                    e.printStackTrace(System.err);
-                }
-                System.err.println("*** server shut down");
-            }
-        });
+    @Override
+    protected int getPort_() {
+        return configMgr().getConfigInteger(CFG_KEY_PORT, DEFAULT_PORT);
     }
 
-    protected void stop() throws InterruptedException {
-        if (server != null) {
-            server.shutdown().awaitTermination(TIMEOUT_TERMINATION_SECS, TimeUnit.SECONDS);
-        }
-    }
-
-    /**
-     * Await termination on the main thread since the grpc library uses daemon threads.
-     */
-    protected void blockUntilShutdown() throws InterruptedException {
-        if (server != null) {
-            server.awaitTermination();
-        }
-        finiService();
-    }
-
-    private void initService() {
+    @Override
+    protected void initService_() {
 
         // create and initialize handler
         QueryHandlerInterface handler = MongoQueryHandler.newMongoSyncQueryHandler();
-        LOGGER.info("initService using handler: " + handler.getClass().getName());
+        LOGGER.info("initService_ using handler: " + handler.getClass().getName());
 
         // create and initialize ingestion service implementation
-        serviceImpl = new QueryServiceImpl();
         if (!serviceImpl.init(handler)) {
-            LOGGER.error("initService serviceImpl.init failed");
+            LOGGER.error("initService_ serviceImpl.init failed");
             return;
         }
     }
 
-    private void finiService() {
+    @Override
+    protected void finiService_() {
         if (serviceImpl != null) {
             serviceImpl.fini();
         }
     }
 
-    /**
-     * Main launches the server from the command line.
-     */
     public static void main(String[] args) throws IOException, InterruptedException {
 
         // Note that config overrides passed on the command line must be set using "-D" as VM arguments so that they
         // appear on the command line before the main class.  Otherwise, they are passed as arguments in argv to main.
         // Example: "java -Ddp.GrpcServer.port=50052 com.ospreydcs.dp.query.server.QueryGrpcServer".
 
-        final QueryGrpcServer server = new QueryGrpcServer();
+        QueryServiceImpl serviceImpl = new QueryServiceImpl();
+        final QueryGrpcServer server = new QueryGrpcServer(serviceImpl);
         server.start();
         server.blockUntilShutdown();
     }
