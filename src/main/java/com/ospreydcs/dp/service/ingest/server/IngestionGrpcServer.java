f75d84b5e15c609f5996cc0247c806fa1c389507
Craig McChesney <auto@craigmcc.com>
2024-06-11 14:49:53 -0600

    dp-service #72: refactor ingestion server to extend GrpcServerBase framework

--- src/main/java/com/ospreydcs/dp/service/ingest/server/IngestionGrpcServer.java
+++ src/main/java/com/ospreydcs/dp/service/ingest/server/IngestionGrpcServer.java
@@ -1,109 +1,64 @@
 package com.ospreydcs.dp.service.ingest.server;
 
-import com.ospreydcs.dp.service.common.config.ConfigurationManager;
+import com.ospreydcs.dp.service.common.server.GrpcServerBase;
 import com.ospreydcs.dp.service.ingest.handler.interfaces.IngestionHandlerInterface;
 import com.ospreydcs.dp.service.ingest.handler.mongo.MongoIngestionHandler;
 import com.ospreydcs.dp.service.ingest.service.IngestionServiceImpl;
-import io.grpc.Grpc;
-import io.grpc.InsecureServerCredentials;
-import io.grpc.Server;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
-public class IngestionGrpcServer {
+public class IngestionGrpcServer extends GrpcServerBase {
 
     private static final Logger LOGGER = LogManager.getLogger();
-
-    // constants
-    private static final int TIMEOUT_TERMINATION_SECS = 30;
 
     // configuration
     public static final String CFG_KEY_PORT = "IngestionServer.port";
     public static final int DEFAULT_PORT = 50051;
 
-    private Server server;
-    private IngestionServiceImpl serviceImpl;
+    // instance variables
+    private final IngestionServiceImpl serviceImpl;
 
-    private static ConfigurationManager configMgr() {
-        return ConfigurationManager.getInstance();
+    public IngestionGrpcServer(IngestionServiceImpl serviceImpl) {
+        super(serviceImpl);
+        this.serviceImpl = serviceImpl;
     }
 
-    protected void start() throws IOException {
-
-        initService();
-
-        int port = configMgr().getConfigInteger(CFG_KEY_PORT, DEFAULT_PORT);
-        server = Grpc.newServerBuilderForPort(port, InsecureServerCredentials.create())
-                .addService(serviceImpl)
-                .build()
-                .start();
-        LOGGER.info("Server started, listening on " + port);
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            @Override
-            public void run() {
-                // Use stderr here since the logger may have been reset by its JVM shutdown hook.
-                System.err.println("*** shutting down gRPC server since JVM is shutting down");
-                try {
-                    IngestionGrpcServer.this.stop();
-                } catch (InterruptedException e) {
-                    e.printStackTrace(System.err);
-                }
-                System.err.println("*** server shut down");
-            }
-        });
+    @Override
+    protected int getPort_() {
+        return configMgr().getConfigInteger(CFG_KEY_PORT, DEFAULT_PORT);
     }
 
-    protected void stop() throws InterruptedException {
-        if (server != null) {
-            server.shutdown().awaitTermination(TIMEOUT_TERMINATION_SECS, TimeUnit.SECONDS);
-        }
-    }
+    @Override
+    protected void initService_() {
 
-    /**
-     * Await termination on the main thread since the grpc library uses daemon threads.
-     */
-    protected void blockUntilShutdown() throws InterruptedException {
-        if (server != null) {
-            server.awaitTermination();
-        }
-        finiService();
-    }
-
-    private void initService() {
-
-        // create and initialize db handler
+        // create and initialize handler
         IngestionHandlerInterface handler = MongoIngestionHandler.newMongoSyncIngestionHandler();
-//        IngestionHandlerInterface handler = MongoIngestionHandler.newMongoAsyncIngestionHandler();
-        LOGGER.info("initService using handler: " + handler.getClass().getName());
+        LOGGER.info("initService_ using handler: " + handler.getClass().getName());
 
         // create and initialize ingestion service implementation
-        serviceImpl = new IngestionServiceImpl();
         if (!serviceImpl.init(handler)) {
-            LOGGER.error("initService serviceImpl.init failed");
+            LOGGER.error("initService_ serviceImpl.init failed");
             return;
         }
-
     }
 
-    private void finiService() {
+    @Override
+    protected void finiService_() {
         if (serviceImpl != null) {
             serviceImpl.fini();
         }
     }
 
-    /**
-     * Main launches the server from the command line.
-     */
     public static void main(String[] args) throws IOException, InterruptedException {
 
         // Note that config overrides passed on the command line must be set using "-D" as VM arguments so that they
         // appear on the command line before the main class.  Otherwise, they are passed as arguments in argv to main.
         // Example: "java -Ddp.GrpcServer.port=50052 com.ospreydcs.dp.ingest.server.IngestionGrpcServer".
 
-        final IngestionGrpcServer server = new IngestionGrpcServer();
+        IngestionServiceImpl serviceImpl = new IngestionServiceImpl();
+        final IngestionGrpcServer server = new IngestionGrpcServer(serviceImpl);
         server.start();
         server.blockUntilShutdown();
     }
