86328569a64e3ebe062204d4472ea13b3b05584c
Craig McChesney <auto@craigmcc.com>
2024-10-01 14:19:08 -0600

    dp-service #107: add handling for additional bucket document fields to DatasetExportHdf5File

--- src/main/java/com/ospreydcs/dp/service/annotation/utility/DatasetExportHdf5File.java
+++ src/main/java/com/ospreydcs/dp/service/annotation/utility/DatasetExportHdf5File.java
@@ -7,8 +7,52 @@ import com.ospreydcs.dp.service.common.bson.bucket.BucketDocument;
 import java.io.File;
 import java.io.IOException;
 
+/*
+ * Export file directory structure (using hdf5 groups):
+ *
+ * /root/dataset : Contains details about the dataset exported to this file, with list of pvs and begin/end times
+ * for each data block (TODO).
+ *
+ * /root/pvs : Contains index structure by pv name and bucket first time (seconds and nanos), where each leaf directory
+ * contains the bucket document fields for the bucket whose first time matches the directory path's seconds and nanos.
+ * E.g., "/pvs/S01-GCC01/times/001727467265/000353987280" contains fields for the S01-GCC01 bucket document whose first
+ * time seconds is 001727467265 and nanos 000353987280.
+ *
+ * /root/times : Contains index structure by 1) the first time seconds and nanos fields of the dataset's buckets and
+ * 2) the pv names with buckets whose first time matches the path's seconds and nanos. The entry for each pv name is a
+ * soft link to the directory containing the bucket document fields for the bucket for that pv whose first time matches
+ * the path name's seconds and nanos.  E.g., "/root/times/001727467265/000353987280/pvs/S01-GCC01" is linked to
+ * "/pvs/S01-GCC01/times/001727467265/000353987280", so the bucket fields described above are also available
+ * by navigating the soft link.
+ */
+
 public class DatasetExportHdf5File {
 
+    // constants
+    private final static String GROUP_DATASET = "dataset";
+    private final static String GROUP_PVS = "pvs";
+    private final static String GROUP_TIMES = "times";
+    private final static String DATASET_FIRST_SECONDS = "firstSeconds";
+    private final static String DATASET_FIRST_NANOS = "firstNanos";
+    private final static String DATASET_FIRST_TIME = "firstTime";
+    private final static String DATASET_LAST_SECONDS = "lastSeconds";
+    private final static String DATASET_LAST_NANOS = "lastNanos";
+    private final static String DATASET_LAST_TIME = "lastTime";
+    private final static String DATASET_SAMPLE_COUNT = "sampleCount";
+    private final static String DATASET_SAMPLE_PERIOD = "samplePeriod";
+    private final static String DATASET_DATA_COLUMN_BYTES = "dataColumnBytes";
+    private final static String DATASET_DATA_TIMESTAMPS_BYTES = "dataTimestampsBytes";
+    private final static String DATASET_ATTRIBUTE_MAP_KEYS = "attributeMapKeys";
+    private final static String DATASET_ATTRIBUTE_MAP_VALUES = "attributeMapValues";
+    private final static String DATASET_EVENT_METADATA_DESCRIPTION = "eventMetadataDescription";
+    private final static String DATASET_EVENT_METADATA_START_SECONDS = "eventMetadataStartSeconds";
+    private final static String DATASET_EVENT_METADATA_START_NANOS = "eventMetadataStartNanos";
+    private final static String DATASET_EVENT_METADATA_STOP_SECONDS = "eventMetadataStopSeconds";
+    private final static String DATASET_EVENT_METADATA_STOP_NANOS = "eventMetadataStopNanos";
+    private final static String DATASET_PROVIDER_ID = "providerId";
+    private final static String PATH_SEPARATOR = "/";
+
+    // instance variables
     private final IHDF5Writer writer;
 
     public DatasetExportHdf5File(String filePathString) throws IOException {
@@ -27,64 +71,112 @@ public class DatasetExportHdf5File {
 
     public void createGroups() {
         // create top-level groups for file organization
-        // TODO: use constants
-        writer.object().createGroup("dataset");
-        writer.object().createGroup("pvs");
-        writer.object().createGroup("times");
+        writer.object().createGroup(GROUP_DATASET);
+        writer.object().createGroup(GROUP_PVS);
+        writer.object().createGroup(GROUP_TIMES);
     }
 
     public void writeBucketData(BucketDocument bucketDocument) {
 
-//        - /root/dataset/[list of data block pojo]
-//        - where each data block pojo contains list of PVs and time range begin/end times, ordered by time
-//        - /root/pvs/[S01_GCC01, …]/times/[T01, T02, T03, …]/bucket
-//                - bucket for S01_GCC01 starting at time T01
-//                - /root/times/[T01, T02, T03, …]/pvs/[S01_GCC01, …]/bucket
-//                -  bucket for S01_GCC01 starting at time T01
-//                - points to same object as /root/pvs/… entry
-
-        // create pv-specific groups in hdf5 file
-        final String pvNameGroup = "pvs/" + bucketDocument.getPvName();
+        // create groups for indexing by pv and time
+        final String pvNameGroup = GROUP_PVS + PATH_SEPARATOR + bucketDocument.getPvName();
         if (! writer.object().isGroup(pvNameGroup)) {
             writer.object().createGroup(pvNameGroup);
         }
-        final String pvTimesGroup = pvNameGroup + "/times";
+        final String pvTimesGroup = pvNameGroup + PATH_SEPARATOR + GROUP_TIMES;
         if (! writer.object().isGroup(pvTimesGroup)) {
             writer.object().createGroup(pvTimesGroup);
         }
         final String firstSecondsString = String.format("%012d", bucketDocument.getFirstSeconds());
-        final String pvTimesSecondsGroup = pvTimesGroup + "/" + firstSecondsString;
+        final String pvTimesSecondsGroup = pvTimesGroup + PATH_SEPARATOR + firstSecondsString;
         if (! writer.object().isGroup(pvTimesSecondsGroup)) {
             writer.object().createGroup(pvTimesSecondsGroup);
         }
         final String firstNanosString = String.format("%012d", bucketDocument.getFirstNanos());
-        final String pvTimesSecondsNanosGroup = pvTimesSecondsGroup + "/" + firstNanosString;
+        final String pvTimesSecondsNanosGroup = pvTimesSecondsGroup + PATH_SEPARATOR + firstNanosString;
         if (! writer.object().isGroup(pvTimesSecondsNanosGroup)) {
             writer.object().createGroup(pvTimesSecondsNanosGroup);
         }
 
-        // write fields from bucket document (including column data values) document under pv-specific path
-        final String bucketDataPath = pvTimesSecondsNanosGroup + "/data";
-        writer.writeByteArray(bucketDataPath, bucketDocument.getDataColumnBytes());
+        // write fields from bucket document (including column data values) document under pv index
 
-        // create timestamp-specific group in hdf5 file
-        final String timesSecondsGroup = "times/" + firstSecondsString;
+        // first seconds/nanos/time
+        final String firstSecondsPath = pvTimesSecondsNanosGroup + PATH_SEPARATOR + DATASET_FIRST_SECONDS;
+        writer.writeLong(firstSecondsPath, bucketDocument.getFirstSeconds());
+        final String firstNanosPath = pvTimesSecondsNanosGroup + PATH_SEPARATOR + DATASET_FIRST_NANOS;
+        writer.writeLong(firstNanosPath, bucketDocument.getFirstNanos());
+        final String firstTimePath = pvTimesSecondsNanosGroup + PATH_SEPARATOR + DATASET_FIRST_TIME;
+        writer.time().write(firstTimePath, bucketDocument.getFirstTime());
+
+        // last seconds/nanos/time
+        final String lastSecondsPath = pvTimesSecondsNanosGroup + PATH_SEPARATOR + DATASET_LAST_SECONDS;
+        writer.writeLong(lastSecondsPath, bucketDocument.getLastSeconds());
+        final String lastNanosPath = pvTimesSecondsNanosGroup + PATH_SEPARATOR + DATASET_LAST_NANOS;
+        writer.writeLong(lastNanosPath, bucketDocument.getLastNanos());
+        final String lastTimePath = pvTimesSecondsNanosGroup + PATH_SEPARATOR + DATASET_LAST_TIME;
+        writer.time().write(lastTimePath, bucketDocument.getLastTime());
+
+        // sample period and count
+        final String sampleCountPath = pvTimesSecondsNanosGroup + PATH_SEPARATOR + DATASET_SAMPLE_COUNT;
+        writer.writeInt(sampleCountPath, bucketDocument.getSampleCount());
+        final String samplePeriodPath = pvTimesSecondsNanosGroup + PATH_SEPARATOR + DATASET_SAMPLE_PERIOD;
+        writer.writeLong(samplePeriodPath, bucketDocument.getSamplePeriod());
+
+        // dataColumnBytes
+        final String columnDataPath = pvTimesSecondsNanosGroup + PATH_SEPARATOR + DATASET_DATA_COLUMN_BYTES;
+        writer.writeByteArray(columnDataPath, bucketDocument.getDataColumnBytes());
+
+        // dataTimestampsBytes
+        final String dataTimestampsPath = pvTimesSecondsNanosGroup + PATH_SEPARATOR + DATASET_DATA_TIMESTAMPS_BYTES;
+        writer.writeByteArray(dataTimestampsPath, bucketDocument.getDataTimestampsBytes());
+
+        // attributeMap - write keys to one array and values to another
+        final String attributeMapKeysPath = pvTimesSecondsNanosGroup + PATH_SEPARATOR + DATASET_ATTRIBUTE_MAP_KEYS;
+        writer.writeStringArray(attributeMapKeysPath, bucketDocument.getAttributeMap().keySet().toArray(new String[0]));
+        final String attributeMapValuesPath = pvTimesSecondsNanosGroup + PATH_SEPARATOR + DATASET_ATTRIBUTE_MAP_VALUES;
+        writer.writeStringArray(attributeMapValuesPath, bucketDocument.getAttributeMap().values().toArray(new String[0]));
+
+        // eventMetadata - description, start/stop times
+        final String eventMetadataDescriptionPath = 
+                pvTimesSecondsNanosGroup + PATH_SEPARATOR + DATASET_EVENT_METADATA_DESCRIPTION;
+        writer.writeString(eventMetadataDescriptionPath, bucketDocument.getEventMetadata().getDescription());
+        final String eventMetadataStartSecondsPath = 
+                pvTimesSecondsNanosGroup + PATH_SEPARATOR + DATASET_EVENT_METADATA_START_SECONDS;
+        writer.writeLong(eventMetadataStartSecondsPath, bucketDocument.getEventMetadata().getStartSeconds());
+        final String eventMetadataStartNanosPath = 
+                pvTimesSecondsNanosGroup + PATH_SEPARATOR + DATASET_EVENT_METADATA_START_NANOS;
+        writer.writeLong(eventMetadataStartNanosPath, bucketDocument.getEventMetadata().getStartNanos());
+        final String eventMetadataStopSecondsPath =
+                pvTimesSecondsNanosGroup + PATH_SEPARATOR + DATASET_EVENT_METADATA_STOP_SECONDS;
+        writer.writeLong(eventMetadataStopSecondsPath, bucketDocument.getEventMetadata().getStopSeconds());
+        final String eventMetadataStopNanosPath =
+                pvTimesSecondsNanosGroup + PATH_SEPARATOR + DATASET_EVENT_METADATA_STOP_NANOS;
+        writer.writeLong(eventMetadataStopNanosPath, bucketDocument.getEventMetadata().getStopNanos());
+
+        // providerId
+        final String providerIdPath = pvTimesSecondsNanosGroup + PATH_SEPARATOR + DATASET_PROVIDER_ID;
+        writer.writeString(providerIdPath, bucketDocument.getProviderId());
+
+        
+        // create groups for indexing by time and pv
+        final String timesSecondsGroup = GROUP_TIMES + PATH_SEPARATOR + firstSecondsString;
         if (! writer.object().isGroup(timesSecondsGroup)) {
             writer.object().createGroup(timesSecondsGroup);
         }
-        final String timesSecondsNanosGroup = timesSecondsGroup + "/" + firstNanosString;
+        final String timesSecondsNanosGroup = timesSecondsGroup + PATH_SEPARATOR + firstNanosString;
         if (! writer.object().isGroup(timesSecondsNanosGroup)) {
             writer.object().createGroup(timesSecondsNanosGroup);
         }
-        final String timesSecondsNanosPvsGroup = timesSecondsNanosGroup + "/pvs";
+        final String timesSecondsNanosPvsGroup = timesSecondsNanosGroup + PATH_SEPARATOR + GROUP_PVS;
         if (! writer.object().isGroup(timesSecondsNanosPvsGroup)) {
             writer.object().createGroup(timesSecondsNanosPvsGroup);
         }
 
         // create soft link to bucket document under pvs path from times path
-        final String timesSecondsNanosPvsPvPath = timesSecondsNanosPvsGroup + "/" + bucketDocument.getPvName();
+        final String timesSecondsNanosPvsPvPath =
+                timesSecondsNanosPvsGroup + PATH_SEPARATOR + bucketDocument.getPvName();
         if (! writer.object().exists(timesSecondsNanosPvsPvPath)) {
-            writer.object().createSoftLink("/" + pvTimesSecondsNanosGroup, timesSecondsNanosPvsPvPath);
+            writer.object().createSoftLink(PATH_SEPARATOR + pvTimesSecondsNanosGroup, timesSecondsNanosPvsPvPath);
         }
 
 //            writer.string().write("/groupA/string", "Just some random string.");
--- src/test/java/com/ospreydcs/dp/service/annotation/utility/DatasetExportHdf5FileTest.java
+++ src/test/java/com/ospreydcs/dp/service/annotation/utility/DatasetExportHdf5FileTest.java
@@ -1,13 +1,14 @@
 package com.ospreydcs.dp.service.annotation.utility;
 
-import com.ospreydcs.dp.grpc.v1.common.DataColumn;
-import com.ospreydcs.dp.grpc.v1.common.DataValue;
+import com.ospreydcs.dp.grpc.v1.common.*;
 import com.ospreydcs.dp.service.common.bson.bucket.BucketDocument;
+import com.ospreydcs.dp.service.common.bson.bucket.EventMetadataDocument;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.time.Instant;
 import java.util.Date;
+import java.util.Map;
 import java.util.Objects;
 
 import static org.junit.Assert.fail;
@@ -28,21 +29,62 @@ public class DatasetExportHdf5FileTest {
         Objects.requireNonNull(exportHdf5File);
 
         final Instant instantNow = Instant.now();
+        final Instant firstInstant = instantNow.minusNanos(instantNow.getNano());
+        final long firstSeconds = instantNow.getEpochSecond();
+        final long firstNanos = 0;
+        final Date firstTime = Date.from(firstInstant);
+        final long lastSeconds = firstSeconds;
+        final long lastNanos = 900000000L;
+        final Instant lastInstant = firstInstant.plusNanos(lastNanos);
+        final Date lastTime = Date.from(lastInstant);
+
+        final Timestamp samplingClockStartTime = Timestamp.newBuilder()
+                .setEpochSeconds(firstSeconds)
+                .setNanoseconds(firstNanos)
+                .build();
+        final SamplingClock samplingClock =
+                SamplingClock.newBuilder()
+                        .setStartTime(samplingClockStartTime)
+                        .setCount(10)
+                        .setPeriodNanos(100000000L)
+                        .build();
+        final DataTimestamps dataTimestamps = DataTimestamps.newBuilder().setSamplingClock(samplingClock).build();
+
+        final Map<String, String> attributeMap = Map.of("sector", "S01", "subsystem", "vacuum");
+
+        final EventMetadataDocument eventMetadata = new EventMetadataDocument();
+        eventMetadata.setDescription("S01 vacuum test");
+        eventMetadata.setStartSeconds(firstSeconds);
+        eventMetadata.setStartNanos(firstNanos);
+        eventMetadata.setStopSeconds(lastSeconds);
+        eventMetadata.setStopNanos(lastNanos);
+
+        final String providerId = "S01 vacuum provider";
 
         // create first BucketDocument for S01-GCC01
         {
             final BucketDocument bucketDocument = new BucketDocument();
             bucketDocument.setPvName("S01-GCC01");
-            bucketDocument.setFirstSeconds(instantNow.getEpochSecond());
-            bucketDocument.setFirstNanos(instantNow.getNano());
-            DataColumn.Builder dataColumnBuilder = DataColumn.newBuilder();
+            bucketDocument.setFirstSeconds(firstSeconds);
+            bucketDocument.setFirstNanos(firstNanos);
+            bucketDocument.setFirstTime(firstTime);
+            bucketDocument.setLastSeconds(lastSeconds);
+            bucketDocument.setLastNanos(lastNanos);
+            bucketDocument.setLastTime(lastTime);
+            bucketDocument.setSampleCount(10);
+            bucketDocument.setSamplePeriod(100000000L);
+            final DataColumn.Builder dataColumnBuilder = DataColumn.newBuilder();
             dataColumnBuilder.setName(bucketDocument.getPvName());
             for (int i = 0; i < 10; ++i) {
                 DataValue dataValue = DataValue.newBuilder().setIntValue(i).build();
                 dataColumnBuilder.addDataValues(dataValue);
             }
-            DataColumn dataColumn = dataColumnBuilder.build();
+            final DataColumn dataColumn = dataColumnBuilder.build();
             bucketDocument.setDataColumnBytes(dataColumn.toByteArray());
+            bucketDocument.setDataTimestampsBytes(dataTimestamps.toByteArray());
+            bucketDocument.setAttributeMap(attributeMap);
+            bucketDocument.setEventMetadata(eventMetadata);
+            bucketDocument.setProviderId(providerId);
             exportHdf5File.writeBucketData(bucketDocument);
         }
 
@@ -50,16 +92,26 @@ public class DatasetExportHdf5FileTest {
         {
             final BucketDocument bucketDocument = new BucketDocument();
             bucketDocument.setPvName("S01-GCC02");
-            bucketDocument.setFirstSeconds(instantNow.getEpochSecond());
-            bucketDocument.setFirstNanos(instantNow.getNano());
-            DataColumn.Builder dataColumnBuilder = DataColumn.newBuilder();
+            bucketDocument.setFirstSeconds(firstSeconds);
+            bucketDocument.setFirstNanos(firstNanos);
+            bucketDocument.setFirstTime(firstTime);
+            bucketDocument.setLastSeconds(lastSeconds);
+            bucketDocument.setLastNanos(lastNanos);
+            bucketDocument.setLastTime(lastTime);
+            bucketDocument.setSampleCount(10);
+            bucketDocument.setSamplePeriod(100000000L);
+            final DataColumn.Builder dataColumnBuilder = DataColumn.newBuilder();
             dataColumnBuilder.setName(bucketDocument.getPvName());
             for (int i = 10; i < 20; ++i) {
                 DataValue dataValue = DataValue.newBuilder().setIntValue(i).build();
                 dataColumnBuilder.addDataValues(dataValue);
             }
-            DataColumn dataColumn = dataColumnBuilder.build();
+            final DataColumn dataColumn = dataColumnBuilder.build();
             bucketDocument.setDataColumnBytes(dataColumn.toByteArray());
+            bucketDocument.setDataTimestampsBytes(dataTimestamps.toByteArray());
+            bucketDocument.setAttributeMap(attributeMap);
+            bucketDocument.setEventMetadata(eventMetadata);
+            bucketDocument.setProviderId(providerId);
             exportHdf5File.writeBucketData(bucketDocument);
         }
 
