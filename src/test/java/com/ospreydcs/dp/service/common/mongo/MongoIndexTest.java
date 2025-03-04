8c36af5b8732c2d9e8d4d2a08171a9f7a19642c0
Craig McChesney <auto@craigmcc.com>
2025-03-04 14:06:13 -0700

    dp-service #121: fix MongoIndexTest for new indexes on providers collection

--- src/test/java/com/ospreydcs/dp/service/common/mongo/MongoIndexTest.java
+++ src/test/java/com/ospreydcs/dp/service/common/mongo/MongoIndexTest.java
@@ -22,74 +22,107 @@ public class MongoIndexTest {
         public void checkIndexes() {
 
             // check annotations collection indexes
-            final List<Document> annotationsIndexesDocuments =
-                    this.mongoCollectionAnnotations.listIndexes().into(new ArrayList<>());
-            assertEquals(7, annotationsIndexesDocuments.size());
-            final List<String> annotationsIndexNames = annotationsIndexesDocuments.stream()
-                    .map(document -> (String) document.get("name"))
-                    .collect(Collectors.toList());
-            assertTrue(annotationsIndexNames.contains("_id_"));
-            assertTrue(annotationsIndexNames.contains("ownerId_1"));
-            assertTrue(annotationsIndexNames.contains("dataSetIds_1"));
-            assertTrue(annotationsIndexNames.contains("annotationIds_1"));
-            assertTrue(annotationsIndexNames.contains("tags_1"));
-            assertTrue(annotationsIndexNames.contains("attributeMap.$**_1"));
-            assertTrue(annotationsIndexNames.contains("name_text_comment_text_eventMetadata.description_text_ownerId_1"));
+            {
+                final List<Document> annotationsIndexesDocuments =
+                        this.mongoCollectionAnnotations.listIndexes().into(new ArrayList<>());
+                final List<String> annotationsIndexNames = annotationsIndexesDocuments.stream()
+                        .map(document -> (String) document.get("name"))
+                        .collect(Collectors.toList());
+                final List<String> expectedIndexNames =
+                        List.of(
+                                "_id_",
+                                "ownerId_1",
+                                "dataSetIds_1",
+                                "annotationIds_1",
+                                "tags_1",
+                                "attributeMap.$**_1",
+                                "name_text_comment_text_eventMetadata.description_text_ownerId_1"
+                        );
+                assertEquals(expectedIndexNames, annotationsIndexNames);
+            }
 
             // check buckets collection indexes
-            final List<Document> bucketsIndexesDocuments =
-                    this.mongoCollectionBuckets.listIndexes().into(new ArrayList<>());
-            assertEquals(3, bucketsIndexesDocuments.size());
-            final List<String> bucketsIndexNames = bucketsIndexesDocuments.stream()
-                    .map(document -> (String) document.get("name"))
-                    .collect(Collectors.toList());
-            assertTrue(bucketsIndexNames.contains("_id_"));
-            assertTrue(bucketsIndexNames.contains("pvName_1"));
-            assertTrue(bucketsIndexNames.contains("pvName_1_firstSeconds_1_firstNanos_1_lastSeconds_1_lastNanos_1"));
+            {
+                final List<Document> bucketsIndexesDocuments =
+                        this.mongoCollectionBuckets.listIndexes().into(new ArrayList<>());
+                final List<String> bucketsIndexNames = bucketsIndexesDocuments.stream()
+                        .map(document -> (String) document.get("name"))
+                        .collect(Collectors.toList());
+                final List<String> expectedIndexNames =
+                        List.of(
+                                "_id_",
+                                "pvName_1",
+                                "pvName_1_firstSeconds_1_firstNanos_1_lastSeconds_1_lastNanos_1",
+                                "providerId_1"
+                        );
+                assertEquals(expectedIndexNames, bucketsIndexNames);
+            }
 
             // check calculations collection indexes
-            final List<Document> calculationsIndexesDocuments =
-                    this.mongoCollectionCalculations.listIndexes().into(new ArrayList<>());
-            assertEquals(0, calculationsIndexesDocuments.size());
-            final List<String> calculationsIndexNames = calculationsIndexesDocuments.stream()
-                    .map(document -> (String) document.get("name"))
-                    .collect(Collectors.toList());
-            // oddly no indexes are listed for this collection, apparently because 1) it is empty and 2) no other indexes exist
-//            assertTrue(calculationsIndexNames.contains("_id_"));
+            {
+                final List<Document> calculationsIndexesDocuments =
+                        this.mongoCollectionCalculations.listIndexes().into(new ArrayList<>());
+                final List<String> calculationsIndexNames = calculationsIndexesDocuments.stream()
+                        .map(document -> (String) document.get("name"))
+                        .collect(Collectors.toList());
+                final List<String> expectedIndexNames = new ArrayList<>();
+                assertEquals(expectedIndexNames, calculationsIndexNames);
+                // NOTE: oddly no indexes are listed for this collection,
+                // apparently because 1) it is empty and 2) no other indexes exist
+                // assertTrue(calculationsIndexNames.contains("_id_"));
+            }
 
             // check datasets collection indexes
-            final List<Document> datasetsIndexesDocuments =
-                    this.mongoCollectionDataSets.listIndexes().into(new ArrayList<>());
-            assertEquals(4, datasetsIndexesDocuments.size());
-            final List<String> datasetsIndexNames = datasetsIndexesDocuments.stream()
-                    .map(document -> (String) document.get("name"))
-                    .collect(Collectors.toList());
-            assertTrue(datasetsIndexNames.contains("_id_"));
-            assertTrue(datasetsIndexNames.contains("ownerId_1"));
-            assertTrue(datasetsIndexNames.contains("name_text_description_text_ownerId_1"));
-            assertTrue(datasetsIndexNames.contains("dataBlocks.pvNames_1"));
+            {
+                final List<Document> datasetsIndexesDocuments =
+                        this.mongoCollectionDataSets.listIndexes().into(new ArrayList<>());
+                final List<String> datasetsIndexNames = datasetsIndexesDocuments.stream()
+                        .map(document -> (String) document.get("name"))
+                        .collect(Collectors.toList());
+                final List<String> expectedIndexNames =
+                        List.of(
+                                "_id_",
+                                "ownerId_1",
+                                "name_text_description_text_ownerId_1",
+                                "dataBlocks.pvNames_1"
+                        );
+                assertEquals(expectedIndexNames, datasetsIndexNames);
+            }
 
             // check providers collection indexes
-            final List<Document> providersIndexesDocuments =
-                    this.mongoCollectionProviders.listIndexes().into(new ArrayList<>());
-            assertEquals(2, providersIndexesDocuments.size());
-            final List<String> providersIndexNames = providersIndexesDocuments.stream()
-                    .map(document -> (String) document.get("name"))
-                    .collect(Collectors.toList());
-            assertTrue(providersIndexNames.contains("_id_"));
-            assertTrue(providersIndexNames.contains("name_1"));
+            {
+                final List<Document> providersIndexesDocuments =
+                        this.mongoCollectionProviders.listIndexes().into(new ArrayList<>());
+                final List<String> providersIndexNames = providersIndexesDocuments.stream()
+                        .map(document -> (String) document.get("name"))
+                        .collect(Collectors.toList());
+                final List<String> expectedIndexNames =
+                        List.of(
+                                "_id_",
+                                "name_1",
+                                "name_text_description_text",
+                                "tags_1",
+                                "attributeMap.$**_1"
+                        );
+                assertEquals(expectedIndexNames, providersIndexNames);
+            }
 
             // check requestStatus collection indexes
-            final List<Document> requestStatusIndexesDocuments =
-                    this.mongoCollectionRequestStatus.listIndexes().into(new ArrayList<>());
-            assertEquals(4, requestStatusIndexesDocuments.size());
-            final List<String> requestStatusIndexNames = requestStatusIndexesDocuments.stream()
-                    .map(document -> (String) document.get("name"))
-                    .collect(Collectors.toList());
-            assertTrue(requestStatusIndexNames.contains("_id_"));
-            assertTrue(requestStatusIndexNames.contains("providerId_1_requestId_1"));
-            assertTrue(requestStatusIndexNames.contains("providerId_1_requestStatusCase_1_updateTime_1"));
-            assertTrue(requestStatusIndexNames.contains("requestStatusCase_1_updateTime_1"));
+            {
+                final List<Document> requestStatusIndexesDocuments =
+                        this.mongoCollectionRequestStatus.listIndexes().into(new ArrayList<>());
+                final List<String> requestStatusIndexNames = requestStatusIndexesDocuments.stream()
+                        .map(document -> (String) document.get("name"))
+                        .collect(Collectors.toList());
+                final List<String> expectedIndexNames =
+                        List.of(
+                                "_id_",
+                                "providerId_1_requestId_1",
+                                "providerId_1_requestStatusCase_1_updateTime_1",
+                                "requestStatusCase_1_updateTime_1"
+                        );
+                assertEquals(expectedIndexNames, requestStatusIndexNames);
+            }
         }
     }
 
