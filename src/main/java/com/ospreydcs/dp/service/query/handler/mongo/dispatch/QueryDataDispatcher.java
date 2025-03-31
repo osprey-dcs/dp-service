5a80f28040a020a092d6a30c6095675fdd328ab1
Craig McChesney <auto@craigmcc.com>
2025-03-31 15:50:35 -0500

    dp-service #136: improvements to data query result handling

--- src/main/java/com/ospreydcs/dp/service/query/handler/mongo/dispatch/QueryDataDispatcher.java
+++ src/main/java/com/ospreydcs/dp/service/query/handler/mongo/dispatch/QueryDataDispatcher.java
@@ -3,14 +3,19 @@ package com.ospreydcs.dp.service.query.handler.mongo.dispatch;
 import com.mongodb.client.MongoCursor;
 import com.ospreydcs.dp.grpc.v1.query.QueryDataResponse;
 import com.ospreydcs.dp.service.common.bson.bucket.BucketDocument;
+import com.ospreydcs.dp.service.common.exception.DpException;
+import com.ospreydcs.dp.service.query.handler.mongo.MongoQueryHandler;
 import com.ospreydcs.dp.service.query.service.QueryServiceImpl;
 import io.grpc.stub.StreamObserver;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Objects;
+
 public class QueryDataDispatcher extends QueryDataAbstractDispatcher {
 
-    private static final Logger LOGGER = LogManager.getLogger();
+    // static variables
+    private static final Logger logger = LogManager.getLogger();
 
     public QueryDataDispatcher(StreamObserver<QueryDataResponse> responseObserver) {
         super(responseObserver);
@@ -19,21 +24,67 @@ public class QueryDataDispatcher extends QueryDataAbstractDispatcher {
     @Override
     public void handleResult_(MongoCursor<BucketDocument> cursor) {
 
-        QueryDataResponse response = super.nextQueryResponseFromCursor(cursor);
+        // build response from query result cursor
+        final QueryDataResponse.QueryData.Builder queryDataBuilder =
+                QueryDataResponse.QueryData.newBuilder();
+        int messageSize = 0;
 
-        if (response == null) {
-            final String msg = "unexpected error building QueryResponse from cursor";
-            QueryServiceImpl.sendQueryDataResponseError(msg, getResponseObserver());
+        while (cursor.hasNext()){
 
-        } else if (cursor.hasNext()) {
-            // query returned more data than will fit in a single response, so return an error instead of partial data
-            final String msg = "query returned more data than will fit in single QueryResponse message";
-            QueryServiceImpl.sendQueryDataResponseError(msg, getResponseObserver());
+            final BucketDocument document = cursor.next();
+            QueryDataResponse.QueryData.DataBucket bucket = null;
+            try {
+                bucket = BucketDocument.dataBucketFromDocument(document);
+            } catch (DpException e) {
+                // exception deserializing BucketDocument contents, so send error response
+                final String errorMsg =
+                        "exception deserializing protobuf data for BucketDocument id: "
+                                + getResponseObserver().hashCode()
+                                + " exception: " + e.getMessage();
+                logger.error(errorMsg);
+                QueryServiceImpl.sendQueryDataResponseError(errorMsg, getResponseObserver());
+                return;
+            }
+            Objects.requireNonNull(bucket);
 
-        } else {
-            getResponseObserver().onNext(response);
-            getResponseObserver().onCompleted();
+            // determine bucket size and check if too large
+            int bucketSerializedSize = bucket.getSerializedSize();
+            if (bucketSerializedSize > MongoQueryHandler.getOutgoingMessageSizeLimitBytes()) {
+                // single bucket is larger than maximum message size, so send error response
+                QueryServiceImpl.sendQueryDataResponseError(
+                        "bucket size: " + bucketSerializedSize
+                                + " greater than maximum message size: "
+                                + MongoQueryHandler.getOutgoingMessageSizeLimitBytes(),
+                        getResponseObserver());
+                return;
+            }
+
+            // add bucket to result
+            queryDataBuilder.addDataBuckets(bucket);
+            messageSize = messageSize + bucketSerializedSize;
+
+            // break out of cursor handling loop if next bucket might exceed maximum size
+            if (messageSize > MongoQueryHandler.getOutgoingMessageSizeLimitBytes()) {
+                QueryServiceImpl.sendQueryDataResponseError(
+                        "query returned more data than will fit in single QueryResponse message",
+                        getResponseObserver()
+                );
+                return;
+            }
         }
+
+        // create response
+        QueryDataResponse response;
+        if (messageSize > 0) {
+            // create response from buckets in result
+            response = QueryServiceImpl.queryDataResponse(queryDataBuilder);
+        } else {
+            // create response with empty query result
+            response = QueryDataResponse.newBuilder().build();
+        }
+
+        getResponseObserver().onNext(response);
+        getResponseObserver().onCompleted();
 
         cursor.close();
     }
