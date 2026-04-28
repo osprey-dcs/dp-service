0b863dc07f834f68ac4a06cad2e5872d9ad998fd
Craig McChesney <auto@craigmcc.com>
2026-04-28 13:28:59 -0600

    dp-service #177: dp-service #177: fix intermittent test failure for column metadata attributes

--- src/test/integration/java/com/ospreydcs/dp/service/integration/v2api/IngestDataColumnMetadataIT.java
+++ src/test/integration/java/com/ospreydcs/dp/service/integration/v2api/IngestDataColumnMetadataIT.java
@@ -89,6 +89,7 @@ public class IngestDataColumnMetadataIT extends GrpcIntegrationTestBase {
         IngestDataRequest request = IngestionTestBase.buildIngestionRequest(params);
         List<BucketDocument> buckets = ingestionServiceWrapper.sendAndVerifyIngestData(params, request);
 
+
         assertEquals(1, buckets.size());
         GrpcIntegrationIngestionServiceWrapper.verifyBucketColumnMetadata(buckets.get(0), metadata);
 
